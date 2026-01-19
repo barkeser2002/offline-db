035dd8e9cfcca26f101e159fc7ac3494dbe4a386
Barış Keser <45911502+barkeser2002@users.noreply.github.com>
2026-01-19 16:17:54 +0300

    fix(settings): correct dictionary key quotes in database options

--- aniscrap_core/settings.py
+++ aniscrap_core/settings.py
@@ -122,7 +122,7 @@ else:
             "PASSWORD": os.getenv('DB_PASSWORD', ''),
             "HOST": os.getenv('DB_HOST', '127.0.0.1'),
             "PORT": os.getenv('DB_PORT', '3306'),
-            "OPTIONS": {
+            'OPTIONS': {
                 'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
                 'charset': 'utf8mb4',
             },
